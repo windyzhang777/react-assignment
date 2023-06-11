48ec4efbd66eaec8d1791259528bbe296cb26cb4
Windy Zhang <windy.zhang777@hotmail.com>
2023-06-10 19:44:04 -0500

    added table sorting

--- my-app/src/components/DataTable.js
+++ my-app/src/components/DataTable.js
@@ -1,5 +1,10 @@
 import InfoIcon from "@mui/icons-material/Info";
-import { Box, Tooltip } from "@mui/material";
+import {
+  Box,
+  TablePagination,
+  TableSortLabel,
+  Tooltip,
+} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,53 +12,152 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { visuallyHidden } from "@mui/utils";
 import {
   useCallback,
   useEffect,
   useMemo,
   useState,
 } from "react";
-
-const TABLE1_HEADING = [
-  "User Id",
-  "Transactions($)",
-  "Created At",
-  "Reward Points",
-];
-
-const TABLE2_HEADING = [
-  "User Id",
-  "Total Points",
-  "March(Points)",
-  "April(Points)",
-  "May(Points)",
-];
+import {
+  getComparator,
+  stableSort,
+} from "../helpers/stableSort";
 
 const REWARD_DISCLAIMER = `
 A customer receives 2 points for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent between $50 and $100 in each transaction.
 (e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
 `;
 
+const TABLE1_HEADING = [
+  {
+    id: "id",
+    numeric: false,
+    disablePadding: true,
+    label: "User Id",
+  },
+  {
+    id: "amount",
+    numeric: true,
+    disablePadding: true,
+    label: "Transactions($)",
+  },
+  {
+    id: "createdat",
+    numeric: true,
+    disablePadding: true,
+    label: "Created At",
+  },
+  {
+    id: "points",
+    numeric: true,
+    disablePadding: true,
+    label: "Reward Points",
+    tooltip: (
+      <Tooltip
+        sx={{ height: "18px", width: "18px" }}
+        title={REWARD_DISCLAIMER}
+        placement="top"
+      >
+        <InfoIcon />
+      </Tooltip>
+    ),
+  },
+];
+
+const TABLE2_HEADING = [
+  {
+    id: "id",
+    numeric: false,
+    disablePadding: true,
+    label: "User Id",
+  },
+  {
+    id: "total",
+    numeric: true,
+    disablePadding: true,
+    label: "Total Points",
+  },
+  {
+    id: "march",
+    numeric: true,
+    disablePadding: true,
+    label: "March(Points)",
+  },
+  {
+    id: "april",
+    numeric: true,
+    disablePadding: true,
+    label: "April(Points)",
+  },
+  {
+    id: "may",
+    numeric: true,
+    disablePadding: true,
+    label: "May(Points)",
+  },
+];
+
 export function DataTable({
   customerData,
   isTx,
-  getSortedPointsArr,
+  getDataByPoints,
   sortUsers,
 }) {
   // TODO: table sorting
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("id");
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleRequestSort = useCallback(
+    (event, property) => {
+      const isAsc = orderBy === property && order === "asc";
+      setOrder(isAsc ? "desc" : "asc");
+      setOrderBy(property);
+    },
+    [order, orderBy]
+  );
+
+  const createSortHandler = useCallback(
+    (property) => (event) => {
+      handleRequestSort(event, property);
+    },
+    [handleRequestSort]
+  );
+
+  const visibleRows = useCallback(
+    (data) =>
+      stableSort(
+        data,
+        getComparator(order, orderBy)
+      )?.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [orderBy, page, rowsPerPage, order]
+  );
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const handleDataTransform = useCallback(
     (data) => {
       const allUserIds = sortUsers(data);
       const res = [];
       for (const id of allUserIds) {
-        const arr = getSortedPointsArr(id);
-        res.push({ id: id, points: arr });
+        res.push(getDataByPoints(id));
       }
       return res;
     },
-    [getSortedPointsArr, sortUsers]
+    [getDataByPoints, sortUsers]
   );
 
   const tableHeading = useMemo(
@@ -64,84 +168,109 @@ export function DataTable({
   const tableHeadingContent = useMemo(
     () => (
       <TableRow>
-        {tableHeading.map((heading, index) => (
+        {tableHeading?.map((headCell, index) => (
           <TableCell
-            align={index === 0 ? "inherit" : "right"}
             key={index}
+            align={headCell.numeric ? "right" : "left"}
+            padding={
+              headCell.disablePadding ? "none" : "normal"
+            }
+            sortDirection={
+              orderBy === headCell.id ? order : false
+            }
           >
-            {heading === "Reward Points" ? (
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={
+                orderBy === headCell.id ? order : "asc"
+              }
+              onClick={createSortHandler(headCell.id)}
+            >
               <Box
                 sx={{
                   display: "inline-flex",
-                  gap: "10px",
+                  gap: "3px",
+                  alignItems: "center",
                 }}
               >
-                {heading}
-                <Tooltip
-                  title={REWARD_DISCLAIMER}
-                  placement="top"
-                >
-                  <InfoIcon />
-                </Tooltip>
+                {headCell?.label}
+                {!!headCell?.tooltip && headCell.tooltip}
               </Box>
-            ) : (
-              heading
-            )}
+              {orderBy === headCell.id && (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc"
+                    ? "sorted descending"
+                    : "sorted ascending"}
+                </Box>
+              )}
+            </TableSortLabel>
           </TableCell>
         ))}
       </TableRow>
     ),
-    [tableHeading]
+    [createSortHandler, order, orderBy, tableHeading]
   );
 
   const tableBodyContent = useMemo(
     () =>
       isTx
-        ? data?.map((user, index) => (
+        ? visibleRows(data)?.map((row, index) => (
             <TableRow
+              hover
+              tabIndex={-1}
               key={index}
               sx={{
+                cursor: "pointer",
                 "&:last-child td, &:last-child th": {
                   border: 0,
                 },
               }}
             >
               <TableCell component="th" scope="row">
-                {user?.id}
+                {row?.id}
               </TableCell>
               <TableCell align="right">
-                {user?.amount}
+                {row?.amount}
               </TableCell>
               <TableCell align="right">
-                {user?.createdat
+                {row?.createdat
                   .replace(/[TZ]/g, " ")
                   .trim()}
               </TableCell>
               <TableCell align="right">
-                {user?.points}
-              </TableCell>{" "}
+                {row?.points}
+              </TableCell>
             </TableRow>
           ))
-        : handleDataTransform(data)?.map((user, index) => (
-            <TableRow
-              key={index}
-              sx={{
-                "&:last-child td, &:last-child th": {
-                  border: 0,
-                },
-              }}
-            >
-              <TableCell component="th" scope="row">
-                {user?.id}
-              </TableCell>
-              {user?.points?.map((p, index) => (
-                <TableCell key={index} align="right">
-                  {p}
+        : visibleRows(handleDataTransform(data))?.map(
+            (row, index) => (
+              <TableRow
+                key={index}
+                sx={{
+                  "&:last-child td, &:last-child th": {
+                    border: 0,
+                  },
+                }}
+              >
+                <TableCell component="th" scope="row">
+                  {row?.id}
                 </TableCell>
-              ))}
-            </TableRow>
-          )),
-    [isTx, data, handleDataTransform]
+                <TableCell align="right">
+                  {row?.total}
+                </TableCell>
+                <TableCell align="right">
+                  {row?.march}
+                </TableCell>
+                <TableCell align="right">
+                  {row?.april}
+                </TableCell>
+                <TableCell align="right">
+                  {row?.may}
+                </TableCell>
+              </TableRow>
+            )
+          ),
+    [isTx, data, handleDataTransform, visibleRows]
   );
 
   useEffect(() => {
@@ -149,15 +278,26 @@ export function DataTable({
   }, [customerData]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table
-        sx={{ minWidth: 650 }}
-        size="small"
-        aria-label="simple table"
-      >
-        <TableHead>{tableHeadingContent}</TableHead>
-        <TableBody>{tableBodyContent}</TableBody>
-      </Table>
-    </TableContainer>
+    <Paper sx={{ width: "100%", mb: 2 }}>
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ minWidth: 650 }}
+          size="small"
+          aria-label="simple table"
+        >
+          <TableHead>{tableHeadingContent}</TableHead>
+          <TableBody>{tableBodyContent}</TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data?.length ? data?.length : 0}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
