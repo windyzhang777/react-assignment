d10d6e05ab1aecd6e17300db13b3038b3a1e438c
Windy Zhang <windy.zhang777@hotmail.com>
2023-06-19 21:12:55 -0500

    updated hooks on DataChart

--- my-app/src/components/DataChart.js
+++ my-app/src/components/DataChart.js
@@ -7,7 +7,7 @@ import {
   Title,
   Tooltip,
 } from "chart.js";
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
@@ -42,44 +42,49 @@ export function DataChart({
   getDataByPoints,
   sortUsers,
 }) {
-  const handleDataTransform = (data) => {
-    const allUserIds = sortUsers(data);
-    const res = [];
-    for (const id of allUserIds) {
-      const arr = Object.values(getDataByPoints(id))?.slice(
-        1
-      );
-      for (let i = 0; i < arr.length; i++) {
-        if (!res[i]) {
-          res[i] = [arr[i]];
-        } else {
-          res[i].push(arr[i]);
+  const handleDataTransform = useCallback(
+    (data) => {
+      const allUserIds = sortUsers(data);
+      const res = [];
+      for (const id of allUserIds) {
+        const arr = Object.values(
+          getDataByPoints(id)
+        )?.slice(1);
+        for (let i = 0; i < arr.length; i++) {
+          if (!res[i]) {
+            res[i] = [arr[i]];
+          } else {
+            res[i].push(arr[i]);
+          }
         }
       }
-    }
-    return res;
-  };
+      return res;
+    },
+    [getDataByPoints, sortUsers]
+  );
 
-  const data = {
-    labels: sortUsers(customerData),
-    datasets: [
-      {
-        label: "March",
-        data: handleDataTransform(customerData)[1],
-        backgroundColor: "rgb(255, 99, 132)",
-      },
-      {
-        label: "April",
-        data: handleDataTransform(customerData)[2],
-        backgroundColor: "rgb(75, 192, 192)",
-      },
-      {
-        label: "May",
-        data: handleDataTransform(customerData)[3],
-        backgroundColor: "rgb(53, 162, 235)",
-      },
-    ],
-  };
+  const data = useMemo(() => {
+    return {
+      labels: sortUsers(customerData),
+      datasets: [
+        {
+          label: "March",
+          data: handleDataTransform(customerData)[1],
+          backgroundColor: "rgb(255, 99, 132)",
+        },
+        {
+          label: "April",
+          data: handleDataTransform(customerData)[2],
+          backgroundColor: "rgb(75, 192, 192)",
+        },
+        {
+          label: "May",
+          data: handleDataTransform(customerData)[3],
+          backgroundColor: "rgb(53, 162, 235)",
+        },
+      ],
+    };
+  }, [customerData, handleDataTransform, sortUsers]);
 
   return <Bar options={options} data={data} />;
 }
