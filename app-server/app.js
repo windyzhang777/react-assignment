bbd6032e83f4943a2e98fb986aaa21a17460cc3b
Windy Zhang <windy.zhang777@hotmail.com>
2023-06-27 16:40:47 -0500

    added cors router

--- app-server/app.js
+++ app-server/app.js
@@ -1,19 +1,32 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
+const router = express.Router();
 const app = express();
 
-app.get("/getTransactions", (_, res) => {
-  res.sendFile(path.join(__dirname, "/transactions.json"));
-});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+const getTransaction = (_, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, "/transaction.json"));
+};
+router.get("/", getTransaction);
+app.use("/api/transaction", router);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
     console.log(`err :`, err);
-  } else
-    [
-      console.log(
-        `listening on localhost:${process.env.PORT}`
-      ),
-    ];
+  } else {
+    console.log(
+      `listening on localhost:${process.env.PORT}`
+    );
+  }
 });
